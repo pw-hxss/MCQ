340f23fb844ccb478a2e73d7ff78666c865088fc
pw-hxss <124323263+pw-hxss@users.noreply.github.com>
2023-02-28 13:16:59 +0800

    Create question_selector.js

---
+++ index/question_selector.js
@@ -0,0 +1,20 @@
+
+function selectFirst(){
+var firstOption = document.querySelector('input[type="radio"][value="A"]');
+firstOption.checked = true;
+}
+
+function selectSecond(){
+var secondOption = document.querySelector('input[type="radio"][value="B"]');
+secondOption.checked = true;
+}
+
+function selectThird(){
+var thirdOption = document.querySelector('input[type="radio"][value="C"]');
+thirdOption.checked = true;
+}
+
+function selectFourth(){
+var fourthOption = document.querySelector('input[type="radio"][value="D"]');
+fourthOption.checked = true;
+}
