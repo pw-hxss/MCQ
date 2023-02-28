f936fca48652d4fc059f03dd4569381e957f2893
pw-hxss <124323263+pw-hxss@users.noreply.github.com>
2023-02-28 11:31:56 +0800

    Create template.js

---
+++ allQuestions/template.js
@@ -0,0 +1,19 @@
+/*TEMPLATE*/
+// Get all the multiple-choice questions in the page
+var questions = document.querySelectorAll(".CLASS_FOR_MCQ");
+
+// Loop through all the questions
+for (var i = 0; i < questions.length; i++) {
+  // Get all the options for the current question
+  var options = questions[i].querySelectorAll("input[type='radio']");
+
+  // Loop through all the options and select the first one
+  for (var j = 0; j < options.length; j++) {
+    if (options[j].checked) {
+      // If an option is already checked, continue to the next one
+      continue;
+    }
+    options[j].checked = true;
+    break;
+  }
+}
